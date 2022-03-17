6731c3a4aff6852b97c2d5349fe92f3a0fe4622b
suratcoder <100028507+suratcoder@users.noreply.github.com>
2022-03-17 06:28:48 +0300

    Add files via upload

---
+++ figma1/index.js
@@ -0,0 +1,60 @@
+function sayHello(name){
+    let msg = 'Hello, ' + name + '. How are you';
+    return msg;
+}
+console.log(sayHello("SEI2019"));
+
+function sayHello(name){
+    let msg = 'Hello, ' + name + '. How are you';
+    return msg;
+}
+
+console.log(sayHello('Felix'));
+
+function sayHello(name){
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+console.log(sayHello('Muwanguzi'));
+
+function sayHello(name){
+    let msg = 'Hello, ' + name + '. How are you?';
+    return msg;
+}
+console.log(sayHello('software'));
+
+function sayHello(name){
+    let msg = 'Hello, ' + name + ', How are you?';
+    return msg;
+}
+console.log(sayHello('engineer'));
+
+function sayHello(name){
+    let msg = 'Hello, ' + name  + ', How are you?';
+    return msg;
+}
+console.log(sayHello('sixthtime'));
+
+function sayHello(name){
+    let msg = 'Hello, ' + name + ', How are you?';
+    return msg;
+}
+console.log(sayHello('seventhtime'));
+
+function sayHello(name){
+    let msg = 'Hello, ' + name + ', How are you?';
+    return msg;
+}
+console.log(sayHello('eighthtime'));
+
+function sayHello(name){
+    let msg = 'Hello, ' + name + ', How are you?';
+    return msg;
+}
+console.log(sayHello('ninthtime'));
+
+function sayHello(name){
+    let msg = 'Hello, ' + name + ', How are you?';
+    return msg;
+}
+console.log(sayHello('tenthtime'));
